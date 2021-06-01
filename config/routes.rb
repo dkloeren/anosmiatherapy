12e86868f51a9eab3a2bf37b06144177adf1246c
David Kloeren <d.kloeren@googlemail.com>
2021-06-01 10:15:24 +0200

    initit routes, mod seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :scents, only: []
+
+  resources :smell_programs, only: [] do
+    resources :smell_entries, only: []
+  end
+
+  resources :smell_entries, only: []
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ CATEGORIES = %w[fruity spicy etheral]
 RATING = [1,2,3,4,5]
 
 10.times do
-  user = User.create!(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name , email: Faker::Internet.email, password: 111111)
-  scent = Scent.create!(name: Faker::Coffee.blend_name, category: Faker::Coffee.origin)
-  smell_program = SmellProgram.create!(scent: scent, user: user, status: [1,2,3].sample)
-  SmellEntry.create!(strength_rating: RATING.sample, accuracy_rating: RATING.sample, smell_program: smell_program)
-  SmellEntry.create!(strength_rating: RATING.sample, accuracy_rating: RATING.sample, smell_program: smell_program)
+  User.create!(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name , email: Faker::Internet.email, password: "111111")
+  Scent.create!(name: Faker::Coffee.blend_name, category: Faker::Coffee.origin)
+  SmellProgram.create!(scent: Scent.last, user: User.last, status: [1,2,3].sample)
+  SmellEntry.create!(strength_rating: RATING.sample, accuracy_rating: RATING.sample, smell_program: SmellProgram.last)
+  SmellEntry.create!(strength_rating: RATING.sample, accuracy_rating: RATING.sample, smell_program: SmellProgram.last)
 end
