5b62f57640f30f4ac8fd6fdfe571a13f562f5587
David Kloeren <d.kloeren@googlemail.com>
2021-06-01 08:36:06 +0200

    Added validations and relations to models, added active storage

--- app/models/scent.rb
+++ app/models/scent.rb
@@ -1,2 +1,6 @@
 class Scent < ApplicationRecord
+  has_many :smell_programs
+  has_one_attached :photo
+
+  validates :name, presence: true
 end
--- app/models/smell_entry.rb
+++ app/models/smell_entry.rb
@@ -1,3 +1,6 @@
 class SmellEntry < ApplicationRecord
   belongs_to :smell_program
+
+  validates :strength_rating, :accuracy_rating, :smell_program_id, presence: true
+  validates :strength_rating, :accuracy_rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 end
