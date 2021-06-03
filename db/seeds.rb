302f158cc32feade0c4c848cd85904f0ccc48e56
Nayeun Kim <nykim93@hotmail.com>
2021-06-03 20:53:42 +0200

    frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,7 +88,7 @@ CHANGE = [ 0, 0, 0, 1, 1]
 
 User.all.each do |user|
   Scent.all.each do |scent|
-    SmellProgram.create!(scent: scent, user: user, status: "ready")
+    SmellProgram.create!(scent: scent, user: user, status: "ready", image: IMAGES[scent.name])
     strength = INITIAL.sample
     accuracy = INITIAL.sample
     10.times do
