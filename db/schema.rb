e184165e9ece0314a185a0d106f76688b20a99bb
Elisabeth Schaduw <ecaschaduw@gmail.com>
2021-06-07 21:30:14 +0200

    adding info page edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_074710) do
+ActiveRecord::Schema.define(version: 2021_06_07_093413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,13 @@ ActiveRecord::Schema.define(version: 2021_06_07_074710) do
     t.boolean "solved", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "author"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "scents", force: :cascade do |t|
